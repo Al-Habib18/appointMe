c34a9f9e06178a55ae403c388c37fd9837257839
Al-Habib18 <ah18bd@gamil.com>
2024-04-29 14:42:18 +0600

    test: add new tests for testing utils

--- services/auth/test/utils/index.test.ts
+++ services/auth/test/utils/index.test.ts
@@ -1,10 +1,29 @@
 /** @format */
 
-import { generateHash } from "../../src/utils";
+import { generateHash, getAccessToken } from "../../src/utils";
 
+// Test the generateHash function
 it("should generate a hash with salt round of 10", async () => {
     const payload = "test payload";
     const hash = await generateHash(payload);
     expect(hash).toBeDefined();
     expect(typeof hash).toBe("string");
+});
+
+// test the getAccessToken function
+it("should generate a valid JWT access token when user data is provided", () => {
+    // Arrange
+    const userData = {
+        id: "123",
+        email: "test@example.com",
+        name: "John Doe",
+        role: "admin",
+    };
+
+    // Act
+    const accessToken = getAccessToken(userData);
+
+    // Assert
+    expect(accessToken).toBeDefined();
+    expect(typeof accessToken).toBe("string");
 });
